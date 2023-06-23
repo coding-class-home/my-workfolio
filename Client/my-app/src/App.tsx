855ddca7c064aa931b64ea686f98cd968fc8b193
yinkzz <ayinke16@example.com>
2023-06-23 14:21:17 +0100

    create react app

---
+++ Client/my-app/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Showcase Me</h1>
+        <h2>Showcase Me</h2>
+        <h3>Showcase Me</h3>
+        <h4>Showcase Me</h4>
+        <h5>Showcase Me</h5>
+        <h6>Showcase Me</h6>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# my-workfolio
+# Showcase Me
 A simple app to display a short description about me and all my online profile links 
