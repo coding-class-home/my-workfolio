a0895f1b9aed4fb276323174c8b4365fa176ba8c
Damilare Olatubosun <damilareolatubosun@gmail.com>
2023-06-29 23:00:02 +0100

    clean up

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Yinke ft Fash');
+  res.send('Showcase me server');
 });
 
 app.listen(port, () => {
